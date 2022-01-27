1d88389c48aee62f14ab73b1e6a0bb18912d1459
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-01-26 22:36:45 -0500

    Add files via upload

---
+++ Spring_Crud/Crud5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud5Application.class, args);
+	}
+
+}
